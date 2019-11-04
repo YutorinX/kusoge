2a48a505fa9edd65691e3536fde43c6362bd6752
SeiyaIskw <SeiyaIskw@users.noreply.github.com>
2019-11-04 19:16:28 +0900

    Add: move pen and body smoothly

--- src/components/PenFactory/PenBody.tsx
+++ src/components/PenFactory/PenBody.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import penimg from "./img/pen.png";
 
-const styles = (transform: string) => ({
+const styles = (
+  transform: string,
+  right: string | undefined,
+  display: string | undefined
+) => ({
   position: "absolute" as "absolute",
   width: 40,
   height: 388,
 
   top: 129,
-  right: -250,
+  right: right ? right : "-40%",
+  display: display ? display : "block",
   transition: "0.5s ease-in-out",
   transform: transform ? transform : "rotate(90deg)"
 });
@@ -16,25 +21,43 @@ const PenBody: React.FC<{ stage: number; isPenFlipped: boolean }> = ({
   stage,
   isPenFlipped
 }) => {
-  const { transform } = position(stage, isPenFlipped);
-  return <img src={penimg} style={styles(transform)} alt="penimage"></img>;
+  const { transform, right, display } = position(stage, isPenFlipped);
+  return (
+    <img
+      src={penimg}
+      style={styles(transform, right, display)}
+      alt="penimage"
+    ></img>
+  );
 };
 
-function position(stage: number, isPenFlipped: boolean): { transform: string } {
+function position(
+  stage: number,
+  isPenFlipped: boolean
+): { transform: string; right?: string; display?: string } {
   let flip = isPenFlipped ? -180 : 0;
   switch (stage) {
     case 0:
       return { transform: `rotate(${90 + flip}deg)` };
     case 1:
-      return { transform: `rotate(${20 + flip}deg) translate(-400px, 150px)` };
+      return { transform: `rotate(${20 + flip}deg)`, right: "20%" };
     case 2:
-      return { transform: `translateX(-805px) rotate(${flip}deg)` };
+      return {
+        transform: `rotate(${flip}deg)`,
+        right: "50%"
+      };
     case 3:
-      return { transform: `translate(-1185px) rotate(${flip}deg)` };
+      return {
+        transform: `rotate(${flip}deg)`,
+        right: "75%"
+      };
     case 4:
-      return { transform: `translate(-1500px) rotate(${flip}deg)` };
+      return {
+        transform: `rotate(${flip}deg)`,
+        right: "110%"
+      };
     default:
-      return { transform: `translateX(-805px) rotate(${flip}deg)` };
+      return { transform: `rotate(${flip}deg)` };
   }
 }
 
--- src/components/PenFactory/PenCap.tsx
+++ src/components/PenFactory/PenCap.tsx
@@ -1,35 +1,40 @@
 import React from "react";
 import capimg from "./img/cap.png";
 
-const styles = (transform: string) => ({
+const styles = (transform: string, right: string | undefined) => ({
   position: "absolute" as "absolute",
   width: 40,
   height: 114,
 
   zIndex: 10,
   top: 45,
-  left: 555,
+  right: right ? right : "50%",
   transition: "0.5s ease-in-out",
   transform: transform ? transform : ""
 });
 
 const PenCap: React.FC<{ stage: number }> = ({ stage }) => {
-  const trans = position(stage);
+  const { transform, right } = position(stage);
   return (
-    <img src={capimg} style={styles(trans!.transform)} alt="capimage"></img>
+    <img src={capimg} style={styles(transform, right)} alt="capimage"></img>
   );
 };
 
-function position(stage: number) {
+function position(
+  stage: number,
+  right?: string
+): { transform: string; right?: string | undefined } {
   switch (stage) {
     case 0:
       return { transform: "" };
     case 1:
       return { transform: "translateY(70px)" };
     case 2:
-      return { transform: "translate(-375px, 70px)" };
+      return { transform: "translateY(70px)", right: "75%" };
     case 3:
-      return { transform: "translate(-670px, 70px)" };
+      return { transform: "translateY(70px)", right: "110%" };
+    default:
+      return { transform: "" };
   }
 }
 
