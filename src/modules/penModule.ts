a63cf688d5539202039485d3d37dc933647dca1e
SeiyaIskw <SeiyaIskw@users.noreply.github.com>
2019-11-05 04:44:43 +0900

    init: redux modules

---
+++ src/modules/penModule.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "redux-starter-kit";
+
+const initialState: number = 0;
+
+//actions & reducers
+// const penModule = createSlice({
+//   initialState,
+//   reducers: {}
+// });
+
+// export default penModule;
---
+++ src/store.ts
@@ -0,0 +1,20 @@
+// import penModule from "./modules/penModule";
+import {
+  combineReducers,
+  getDefaultMiddleware,
+  configureStore
+} from "redux-starter-kit";
+
+// const rootReducer = combineReducers({
+//   pen: penModule.reducer
+// });
+
+// //setup
+
+// export const setupStore = () => {
+//   const middleware = getDefaultMiddleware();
+//   return configureStore({
+//     reducer: rootReducer,
+//     middleware
+//   });
+// };
