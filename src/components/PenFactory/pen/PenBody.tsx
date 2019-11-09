31bca9abc952257a711cc5e2019886b376adbbc2
SeiyaIskw <SeiyaIskw@users.noreply.github.com>
2019-11-09 22:34:57 +0900

    Refactor: Migrate PenCap inline-style to scss

--- src/components/PenFactory/pen/PenBody.tsx
+++ src/components/PenFactory/pen/PenBody.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import penimg from "./img/pen.png";
+import "./PenBody.scss";
 
-const styles = (
-  transform: string,
-  right: string | undefined,
-  display: string | undefined
-) => ({
-  position: "absolute" as "absolute",
-  width: 40,
-  height: 388,
-
-  top: 129,
-  right: right ? right : "-40%",
+const styles = (transform: string, display: string | undefined) => ({
   display: display ? display : "block",
-  transition: "0.5s ease-in-out",
   transform: transform ? transform : "rotate(90deg)"
 });
 
@@ -21,12 +11,13 @@ const PenBody: React.FC<{ stage: number; isPenFlipped: boolean }> = ({
   stage,
   isPenFlipped
 }) => {
-  const { transform, right, display } = position(stage, isPenFlipped);
+  const { transform, display } = position(stage, isPenFlipped);
   return (
     <img
       src={penimg}
-      style={styles(transform, right, display)}
+      style={styles(transform, display)}
       alt="penimage"
+      className={`penBody pBody${stage}`}
     ></img>
   );
 };
@@ -34,28 +25,19 @@ const PenBody: React.FC<{ stage: number; isPenFlipped: boolean }> = ({
 function position(
   stage: number,
   isPenFlipped: boolean
-): { transform: string; right?: string; display?: string } {
-  let flip = isPenFlipped ? -180 : 0;
+): { transform: string; display?: string } {
+  const flip = isPenFlipped ? -180 : 0;
   switch (stage) {
     case 0:
       return { transform: `rotate(${90 + flip}deg)` };
     case 1:
-      return { transform: `rotate(${20 + flip}deg)`, right: "20%" };
+      return { transform: `rotate(${20 + flip}deg)` };
     case 2:
-      return {
-        transform: `rotate(${flip}deg)`,
-        right: "50%"
-      };
+      return { transform: `rotate(${flip}deg)` };
     case 3:
-      return {
-        transform: `rotate(${flip}deg)`,
-        right: "75%"
-      };
+      return { transform: `rotate(${flip}deg)` };
     case 4:
-      return {
-        transform: `rotate(${flip}deg)`,
-        right: "110%"
-      };
+      return { transform: `rotate(${flip}deg)` };
     default:
       return { transform: `rotate(${flip}deg)` };
   }
