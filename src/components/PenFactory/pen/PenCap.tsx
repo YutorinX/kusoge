2549d2ef77377427c08484281c85799d6f663858
SeiyaIskw <SeiyaIskw@users.noreply.github.com>
2019-11-09 19:24:56 +0900

    Refactor: Migrate PenCap inline-style to scss

--- src/components/PenFactory/pen/PenCap.tsx
+++ src/components/PenFactory/pen/PenCap.tsx
@@ -1,46 +1,11 @@
 import React from "react";
 import capimg from "./img/cap.png";
-
-const styles = (transform: string, right: string | undefined) => ({
-  position: "absolute" as "absolute",
-  width: 40,
-  height: 114,
-
-  zIndex: 10,
-  top: 45,
-  right: right ? right : "50%",
-  transition: "0.5s ease-in-out",
-  transform: transform ? transform : ""
-});
+import "./PenCap.scss";
 
 const PenCap: React.FC<{ stage: number }> = ({ stage }) => {
-  const { transform, right } = position(stage);
   return (
-    <img
-      src={capimg}
-      style={styles(transform, right)}
-      alt="capimage"
-      className="pencap"
-    ></img>
+    <img src={capimg} alt="capimage" className={`penCap cap${stage}`}></img>
   );
 };
-
-function position(
-  stage: number,
-  right?: string
-): { transform: string; right?: string | undefined } {
-  switch (stage) {
-    case 0:
-      return { transform: "" };
-    case 1:
-      return { transform: "translateY(70px)" };
-    case 2:
-      return { transform: "translateY(70px)", right: "75%" };
-    case 3:
-      return { transform: "translateY(70px)", right: "110%" };
-    default:
-      return { transform: "" };
-  }
-}
 
 export default PenCap;
